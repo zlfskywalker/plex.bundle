a7589a6b26fdd10a8d241908b73f5298096b2ce4
lingfei zhu <40218169+zlfskywalker@users.noreply.github.com>
2026-02-16 13:29:26 -0500

    Update javdbAgent.py

--- Contents/Code/javdbAgent.py
+++ Contents/Code/javdbAgent.py
@@ -24,7 +24,7 @@ def getElementFromUrl(url):
 
 
 def request(url):
-    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+   
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
